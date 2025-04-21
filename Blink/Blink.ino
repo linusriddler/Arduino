44b2318a8f21b86c08f7e0de893cb9bc00dbae1e
AaronBushouse <abushouse2@gmail.com>
2025-04-21 14:16:58 -0400

    double LED (yellow/red) - STABLE

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,11 +22,12 @@
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
 int redLED = 12;
+int yellowLED = 11;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 12 as an output.
-  
   pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -34,5 +35,9 @@ void loop() {
   digitalWrite(redLED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(3000);                      // wait for a second
   digitalWrite(redLED, LOW);   // turn the LED off by making the voltage LOW
+  delay(500);                      // wait for a second
+  digitalWrite(yellowLED, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(3000);                      // wait for a second
+  digitalWrite(yellowLED, LOW);   // turn the LED off by making the voltage LOW
   delay(500);                      // wait for a second
 }
