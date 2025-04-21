8b893817d6d4563b4392ac594afa21955668a784
AaronBushouse <abushouse2@gmail.com>
2025-04-21 14:04:47 -0400

    Variable for redLED = 12 - STABLE

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -21,17 +21,18 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
-
+int redLED = 12;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 12 as an output.
-  pinMode(12, OUTPUT);
+  
+  pinMode(redLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(redLED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(3000);                      // wait for a second
-  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(redLED, LOW);   // turn the LED off by making the voltage LOW
   delay(500);                      // wait for a second
 }
